9f508a61e46bee69e8f951cce26c4cf5262dfee3
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-01 20:28:24 +0900

    ブログ Delete機能を追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,8 +10,19 @@ class BlogsController < ApplicationController
   def create
     @blog = Blog.new(blog_params)
     @blog.save
-    redirect_to new_blog_path
+    redirect_to blogs_path
   end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path
+  end
+
 
 
   private
