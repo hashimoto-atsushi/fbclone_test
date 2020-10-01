84cd69a550f046afad52b601ccee42f6fe92dce2
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-01 18:36:50 +0900

    ブログ creatアクション追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,4 +6,16 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.save
+    redirect_to new_blog_path
+  end
+
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 end
