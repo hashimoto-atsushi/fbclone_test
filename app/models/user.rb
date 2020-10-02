7690d931dd60052a4c618a319ec90e07a63c4adb
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-02 11:30:50 +0900

    USERモデル追加 新規DB作成

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :name, presence: true, lenght: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+end
